3ede95e22f9e679a392d0d2b6f9d0d4d89e238af
Polina <polliandfibi@gmail.com>
2024-11-05 23:28:33 +0200

    task refactor

--- src/modules/orders/orders.service.ts
+++ src/modules/orders/orders.service.ts
@@ -1,7 +1,7 @@
 import {Injectable, InternalServerErrorException} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import Order from '../../common/entities/order.entity';
+import Order from 'src/common/entities/order.entity';
 import { CreateOrderDto } from './dto/create-orders.dto';
 
 @Injectable()
